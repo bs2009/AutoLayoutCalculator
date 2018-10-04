e59932e5512b6fd610625dbd6beb608c21bf6dfa
W Song <wmsong@ws-macbook-pro>
2018-10-03 19:28:42 -0500

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by W Song on 10/3/18.
+//  Copyright © 2018 W Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
